e835be8ff9f6f39c29a6588609331c90059b0d60
anar25huseynov <anarhus3ynov@mail.ru>
2023-01-31 21:40:36 +0400

    Java script uzre ilk 7-di task

---
+++ javascript.js
@@ -0,0 +1,118 @@
+// task_1 (+1.2. Input ile daxil olunan ədədlerin cemi.)
+
+// var a = +prompt('Eded dahil edin :')
+// let b = +prompt('Eded dahil edin :')
+// let cem 
+// cem = a + b
+// console.log(+cem)
+
+// task_2 {+1.2. Input ile daxil olunan musbet ədədlerin cemi. } (1)
+
+// let a = +prompt('Musbet Eded dahil edin :')
+// let b = +prompt('Musbet Eded dahil edin :')
+// if (a > 0 && b > 0) {
+
+//     console.log(a+b)
+// }
+// else    {
+//     console.log(' ededlerden biri yada iksi menfidi')
+// }
+
+//  task_2 {+1.2. Input ile daxil olunan musbet ədədlerin cemi. } (2)
+
+// let a = +prompt('Musbet Eded dahil edin :')
+// let b = +prompt('Musbet Eded dahil edin :')
+
+// a > 0 && b > 0 ? console.log(a+b) : console.log('sert odenmedi')
+
+// task_3 (+1. Input ile daxil olunan ədədin sıfır,müsbət yoxsa mənfi olduğunu tapın. )
+
+// let a = +prompt("a ededi daxil edin");
+// let Musbet = a > 0;
+// let Menfi = a < 0;
+// if(Musbet) {
+//     console.log(" Eded musbetti")
+// }
+// else if (Menfi) {
+//     console.log("Eded menfidi")
+// }
+// else {
+//     console.log("Eded sifira bareberdi")
+// }
+
+// task_4 (+1.4. Input ile daxil olunan 2 eded arasinda olan en boyuk ededi tapin)
+
+// let a = prompt('Eded dahil edin')
+// let b = prompt('Eded dahil edin')
+// let Boyuk = a > b
+// let balaca = b > a
+// if(Boyuk) {
+//     console.log("Birinci eded boyukdu")
+//     console.log(a)
+// }
+// else if (balaca) {
+//     console.log("ikinci eded boyukdu")
+//     console.log(b)
+// }
+// else {
+//     console.log("Ededler beraberdi")
+// }
+
+// task_5 {Input ile daxil olunan ədədin cüt yoxsa tək olduğunu tapın.} (1)
+
+// let a = prompt('Eded dahil edin')
+
+// let hesablama = a % 2
+
+//  if (a==0){
+//     console.log("Eded sifirdi")
+// }else if (hesablama == 0){
+//     console.log("Eded cuttur")
+// }else {
+//     console.log("Eded Tekdi")
+// }
+
+// task_5 {Input ile daxil olunan ədədin cüt yoxsa tək olduğunu tapın.} (2)
+
+// let a = prompt('Eded dahil edin')
+
+// let hesablama = a % 2
+
+// a == 0 ? console.log('Eded sifirdi'): hesablama == 0 ? console.log("Eded cuttur") : console.log('Eded Tekdi')
+
+
+// task_6 (Input ile daxil olunan 3 eded arasinda olan en boyuk ededi tapin)
+
+// let a = prompt('Eded dahil edin')
+// let b = prompt('Eded dahil edin')
+// let c = prompt('Eded dahil edin')
+// if (a>b && a>c){
+//     console.log('en boyuk eded birincidi')
+//     console.log(a)
+// }else if (b>a && b>c){
+//     console.log('en boyuk eded ikincidi')
+//     console.log(b)
+// }else if (c>a && c>b){
+//     console.log('en boyuk eded ucuncudu')
+//     console.log(c)
+// }else{
+//     console.log('ededler beraberdi')
+// }
+
+// task_7 (Diskriminantın tapılması )
+
+// console.log(" y = a(x^2) + bx + c")
+// var a = prompt('a ededi dahil edin')
+// var b = prompt('b ededi dahil edin')
+// var c = prompt('c ededi dahil edin')
+// var D = (b**2) - (4 * a * c)
+// var x1 = (-b + ((D)**0,5))/2 * a
+// var x2 = (-b - ((D)**0,5))/2 * a
+// console.log('Diskriminant beraberdi:')
+// console.log(D)
+// console.log('x1 beraberdi:')
+// console.log(x1)
+// console.log('x2 beraberdi:')
+// console.log(x2)
+
+
